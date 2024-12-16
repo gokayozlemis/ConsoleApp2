5374835cc9500e135f25623a583fb993f1c5e60b
gokay <gokay@GOKAY>
2024-12-16 17:41:16 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GokayOzlemis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GokayOzlemis");
+
+            Console.WriteLine("Patika'nin uzman kadrosu ve tecrübeli egitmenleri sayesinde yazılım dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim!");
+
+        }
+    }
+}
+
+  
+
+       
+       
+
+
+
+   
+   
